ef4546cbbc09783659467b7545652cea878880c2
Sonia Singh <89619373+sonia0409@users.noreply.github.com>
2021-12-02 16:29:13 -0500

    Add files via upload

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,28 @@
 const request = require("request");
-const { argv } = require("process");
 
-const breedName = argv.slice(2)[0];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = function(breedName, callback) {
 
-request(url, (error, response, body) => {
-  //if the page or the link is not found
-  if (error) {
-    console.log('Page not found!', error);
-    return;
-  }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  //if breed is  not found
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`${breedName} not found!`);
-  }
+  request(url, (error, response, body) => {
+    //if the page or the link is not found
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    //if breed is  not found
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback(null, `${breedName} not found!`);
+    }
 
-});
+  });
+};
 
+module.exports = { fetchBreedDescription };
 
 
 
